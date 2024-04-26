0a476f94bbdabc8642f95cfc8f4200f14d93dcf0
Kama <mshamratow@gmail.com>
2024-04-27 01:19:13 +0300

    Добавьте файлы проекта.

---
+++ TotalCommander/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace TotalCommander
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel_left = new Panel();
+            button_back_left = new Button();
+            folders_panel_left = new Panel();
+            label3 = new Label();
+            files_panel_left = new Panel();
+            label1 = new Label();
+            label_path = new Label();
+            panel_right = new Panel();
+            button_back_right = new Button();
+            folders_panel_right = new Panel();
+            label4 = new Label();
+            files_panel_right = new Panel();
+            label2 = new Label();
+            label_path2 = new Label();
+            panel_left.SuspendLayout();
+            panel_right.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel_left
+            // 
+            panel_left.BorderStyle = BorderStyle.FixedSingle;
+            panel_left.Controls.Add(button_back_left);
+            panel_left.Controls.Add(folders_panel_left);
+            panel_left.Controls.Add(label3);
+            panel_left.Controls.Add(files_panel_left);
+            panel_left.Controls.Add(label1);
+            panel_left.Controls.Add(label_path);
+            panel_left.ForeColor = Color.LimeGreen;
+            panel_left.Location = new Point(12, 12);
+            panel_left.Name = "panel_left";
+            panel_left.Size = new Size(645, 781);
+            panel_left.TabIndex = 0;
+            panel_left.Paint += panel_left_Paint;
+            // 
+            // button_back_left
+            // 
+            button_back_left.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            button_back_left.Location = new Point(545, 12);
+            button_back_left.Name = "button_back_left";
+            button_back_left.Size = new Size(71, 36);
+            button_back_left.TabIndex = 4;
+            button_back_left.Text = "←";
+            button_back_left.UseVisualStyleBackColor = true;
+            button_back_left.Click += button_back_left_Click;
+            // 
+            // folders_panel_left
+            // 
+            folders_panel_left.Location = new Point(17, 446);
+            folders_panel_left.Name = "folders_panel_left";
+            folders_panel_left.Size = new Size(599, 324);
+            folders_panel_left.TabIndex = 0;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label3.Location = new Point(3, 404);
+            label3.Name = "label3";
+            label3.Size = new Size(78, 30);
+            label3.TabIndex = 3;
+            label3.Text = "Папки:";
+            // 
+            // files_panel_left
+            // 
+            files_panel_left.Location = new Point(17, 99);
+            files_panel_left.Name = "files_panel_left";
+            files_panel_left.Size = new Size(599, 287);
+            files_panel_left.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label1.Location = new Point(3, 57);
+            label1.Name = "label1";
+            label1.Size = new Size(82, 30);
+            label1.TabIndex = 1;
+            label1.Text = "Файлы:";
+            label1.Click += label1_Click;
+            // 
+            // label_path
+            // 
+            label_path.AutoSize = true;
+            label_path.BackColor = Color.DeepSkyBlue;
+            label_path.BorderStyle = BorderStyle.FixedSingle;
+            label_path.Font = new Font("Segoe UI", 14F);
+            label_path.ForeColor = Color.Lime;
+            label_path.Location = new Point(17, 18);
+            label_path.Name = "label_path";
+            label_path.Size = new Size(65, 27);
+            label_path.TabIndex = 0;
+            label_path.Text = "label1";
+            // 
+            // panel_right
+            // 
+            panel_right.BorderStyle = BorderStyle.FixedSingle;
+            panel_right.Controls.Add(button_back_right);
+            panel_right.Controls.Add(folders_panel_right);
+            panel_right.Controls.Add(label4);
+            panel_right.Controls.Add(files_panel_right);
+            panel_right.Controls.Add(label2);
+            panel_right.Controls.Add(label_path2);
+            panel_right.Location = new Point(803, 12);
+            panel_right.Name = "panel_right";
+            panel_right.Size = new Size(642, 781);
+            panel_right.TabIndex = 1;
+            // 
+            // button_back_right
+            // 
+            button_back_right.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            button_back_right.ForeColor = Color.LimeGreen;
+            button_back_right.Location = new Point(554, 12);
+            button_back_right.Name = "button_back_right";
+            button_back_right.Size = new Size(71, 36);
+            button_back_right.TabIndex = 6;
+            button_back_right.Text = "←";
+            button_back_right.UseVisualStyleBackColor = true;
+            button_back_right.Click += button_back_right_Click;
+            // 
+            // folders_panel_right
+            // 
+            folders_panel_right.Location = new Point(16, 446);
+            folders_panel_right.Name = "folders_panel_right";
+            folders_panel_right.Size = new Size(609, 324);
+            folders_panel_right.TabIndex = 5;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label4.ForeColor = Color.LimeGreen;
+            label4.Location = new Point(16, 404);
+            label4.Name = "label4";
+            label4.Size = new Size(78, 30);
+            label4.TabIndex = 4;
+            label4.Text = "Папки:";
+            // 
+            // files_panel_right
+            // 
+            files_panel_right.Location = new Point(16, 99);
+            files_panel_right.Name = "files_panel_right";
+            files_panel_right.Size = new Size(609, 287);
+            files_panel_right.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label2.ForeColor = Color.LimeGreen;
+            label2.Location = new Point(3, 57);
+            label2.Name = "label2";
+            label2.Size = new Size(82, 30);
+            label2.TabIndex = 2;
+            label2.Text = "Файлы:";
+            // 
+            // label_path2
+            // 
+            label_path2.AutoSize = true;
+            label_path2.BackColor = Color.DeepSkyBlue;
+            label_path2.BorderStyle = BorderStyle.FixedSingle;
+            label_path2.Font = new Font("Segoe UI", 14F);
+            label_path2.ForeColor = Color.Lime;
+            label_path2.Location = new Point(16, 18);
+            label_path2.Name = "label_path2";
+            label_path2.Size = new Size(65, 27);
+            label_path2.TabIndex = 1;
+            label_path2.Text = "label1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1482, 795);
+            Controls.Add(panel_right);
+            Controls.Add(panel_left);
+            Name = "Form1";
+            Text = "Form1";
+            panel_left.ResumeLayout(false);
+            panel_left.PerformLayout();
+            panel_right.ResumeLayout(false);
+            panel_right.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel_left;
+        private Panel panel_right;
+        private Label label_path;
+        private Label label_path2;
+        private Label label1;
+        private Label label2;
+        private Panel folders_panel_left;
+        private Label label3;
+        private Panel files_panel_left;
+        private Panel folders_panel_right;
+        private Label label4;
+        private Panel files_panel_right;
+        private Button button_back_left;
+        private Button button_back_right;
+    }
+}
---
+++ TotalCommander/Form1.cs
@@ -0,0 +1,183 @@
+using System.Diagnostics;
+namespace TotalCommander
+{
+    public partial class Form1 : Form
+    {
+        const int max_folders = 1000;
+        const int max_files = 1000;
+
+        const int path_label_size = 100;
+        string current_directory_left = "C:/";
+        string current_directory_right = "C:/";
+        int num_folders_left = 0, num_folders_right = 0;
+        int num_files_left = 0, num_files_right = 0;
+        Label[] folders_left = new Label[max_folders];
+        Label[] folders_right = new Label[max_folders];
+        Label[] files_left = new Label[max_files];
+        Label[] files_right = new Label[max_files];
+        public Form1()
+        {
+            InitializeComponent();
+            ShowDirectory(panel_left, current_directory_left);
+            ShowDirectory(panel_right, current_directory_right);
+        }
+
+        void ChangeUpperLabel(bool left_panel, string path)
+        {
+            string text = String.Format("{0, -" + path_label_size.ToString() + "}", path);
+            if (left_panel)
+            {
+                label_path.Text = text;
+            }
+            else label_path2.Text = text;
+        }
+
+        void ShowDirectory(Panel panel, string directory)
+        {
+            if (panel.Name == "panel_left")
+            {
+                for (int i = 0; i < num_folders_left; i++)
+                {
+                    folders_left[i].Dispose();
+                }
+                for (int i = 0; i < num_files_left; i++)
+                {
+                    files_left[i].Dispose();
+                }
+            }
+            else
+            {
+                for (int i = 0; i < num_folders_right; i++)
+                {
+                    folders_right[i].Dispose();
+                }
+                for (int i = 0; i < num_files_right; i++)
+                {
+                    files_right[i].Dispose();
+                }
+            }
+            if (Directory.Exists(directory))
+            {
+                ChangeUpperLabel(panel.Name == "panel_left", directory);
+                DirectoryInfo di = new DirectoryInfo(directory);
+                int index = 0;
+                int index2 = 0;
+                if (panel.Name == "panel_left")
+                {
+                    foreach (FileInfo fi in di.GetFiles())
+                    {
+                        files_left[index] = new Label { Text = fi.Name, Font = new Font("Arial", 9), Location = new Point(0, index * 20), BackColor = Color.FromName("LightBlue"), ForeColor = Color.FromName("Blue"), BorderStyle = BorderStyle.FixedSingle, Size = new Size(600, 20) };
+                        files_left[index].Parent = files_panel_left;
+                        files_left[index].DoubleClick += file_label_click;
+                        files_left[index].Show();
+                        index++;
+                    }
+                    num_files_left = index;
+
+                    foreach (var fi in di.GetDirectories())
+                    {
+                        folders_left[index2] = new Label { Text = fi.Name, Font = new Font("Arial", 9), Location = new Point(0, index2 * 20), BackColor = Color.FromName("LightBlue"), ForeColor = Color.FromName("Blue"), BorderStyle = BorderStyle.FixedSingle, Size = new Size(600, 20) };
+                        folders_left[index2].Parent = folders_panel_left;
+                        folders_left[index2].DoubleClick += folder_label_click;
+                        folders_left[index2].Show();
+                        index2++;
+                    }
+                    num_folders_left = index2;
+                }
+                else
+                {
+                    foreach (FileInfo fi in di.GetFiles())
+                    {
+                        files_right[index] = new Label { Text = fi.Name, Font = new Font("Arial", 9), Location = new Point(0, index * 20), BackColor = Color.FromName("LightBlue"), ForeColor = Color.FromName("Blue"), BorderStyle = BorderStyle.FixedSingle, Size = new Size(600, 20) };
+                        files_right[index].Parent = files_panel_right;
+                        files_right[index].DoubleClick += file_label_click;
+                        files_right[index].Show();
+                        index++;
+                    }
+                    num_files_right = index;
+
+                    foreach (var fi in di.GetDirectories())
+                    {
+                        folders_right[index2] = new Label { Text = fi.Name, Font = new Font("Arial", 9), Location = new Point(0, index2 * 20), BackColor = Color.FromName("LightBlue"), ForeColor = Color.FromName("Blue"), BorderStyle = BorderStyle.FixedSingle, Size = new Size(600, 20) };
+                        folders_right[index2].Parent = folders_panel_right;
+                        folders_right[index2].DoubleClick += folder_label_click;
+                        folders_right[index2].Show();
+                        index2++;
+                    }
+                    num_folders_right = index2;
+
+                }
+            }
+        }
+
+        private void panel_left_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        void file_label_click(object sender, EventArgs e)
+        {
+            Label label = sender as Label;
+            if (label.Parent == files_panel_left)
+            {
+                ProcessStartInfo psi;
+                string command = $"/k \"{current_directory_left + label.Text}\"";
+                psi = new ProcessStartInfo("cmd", command);
+                psi.UseShellExecute = false;
+                Process.Start(psi);
+            }
+            else
+            {
+                ProcessStartInfo psi;
+                string command = $"/k \"{current_directory_right + label.Text}\"";
+                psi = new ProcessStartInfo("cmd", command);
+                psi.UseShellExecute = false;
+                Process.Start(psi);
+            }
+        }
+
+        void folder_label_click(object sender, EventArgs e)
+        {
+            Label label = sender as Label;
+            if (label.Parent == folders_panel_left)
+            {
+                current_directory_left += label.Text + "/";
+                ShowDirectory(panel_left, current_directory_left);
+            }
+            else
+            {
+                current_directory_right += label.Text + "/";
+                ShowDirectory(panel_right, current_directory_right);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_back_left_Click(object sender, EventArgs e)
+        {
+            string[] folders = current_directory_left.Split("/");
+            string path = "";
+            for (int i = 0; i < folders.Length - 2; i++)
+            {
+                path += folders[i] + "/";
+            }
+            current_directory_left = path;
+            ShowDirectory(panel_left, current_directory_left);
+        }
+
+        private void button_back_right_Click(object sender, EventArgs e)
+        {
+            string[] folders = current_directory_right.Split("/");
+            string path = "";
+            for (int i = 0; i < folders.Length - 2; i++)
+            {
+                path += folders[i] + "/";
+            }
+            current_directory_right = path;
+            ShowDirectory(panel_right, current_directory_right);
+        }
+    }
+}
